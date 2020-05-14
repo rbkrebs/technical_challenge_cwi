b4ed9f59b6ba9eed28b27a782be140bf33b68ae9
Romulo Krebs <romulo.krebs@gmail.com>
2020-05-14 20:09:21 -0300

    criação projeto spring boot

---
+++ cwi/src/main/java/com/cwi/desafio/cwi/CwiApplication.java
@@ -0,0 +1,13 @@
+package com.cwi.desafio.cwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwiApplication.class, args);
+	}
+
+}
---
+++ cwi/src/test/java/com/cwi/desafio/cwi/CwiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwi.desafio.cwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
