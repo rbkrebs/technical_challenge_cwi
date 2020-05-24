fcd3776a66a40a4d2bb259ff7431f774a2c0304c
Romulo Krebs <romulo.krebs@gmail.com>
2020-05-24 16:50:32 -0300

    valida pauta com descrição duplicada

--- cwi/src/main/java/com/cwi/desafio/cwi/business/controller/PautaController.java
+++ cwi/src/main/java/com/cwi/desafio/cwi/business/controller/PautaController.java
@@ -1,5 +1,6 @@
 package com.cwi.desafio.cwi.business.controller;
 
+import com.cwi.desafio.cwi.business.exceptions.PautaExceptionMessage;
 import com.cwi.desafio.cwi.business.model.Pauta;
 import com.cwi.desafio.cwi.business.service.PautaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class PautaController implements ControllerInterface<Pauta> {
     @Override
     public Optional<Pauta> salvar(Pauta pauta) {
 
+        if(this.verificaDescricaoRep(pauta)){
+            throw new IllegalArgumentException(PautaExceptionMessage.FORBIDDEN);
+        }
+
 
         return this.pautaService.salvar(pauta);
     }
@@ -30,7 +35,12 @@ public class PautaController implements ControllerInterface<Pauta> {
 
 
     public Optional<Pauta> buscarPorId(long id) {
-        return pautaService.acharPorId(id);
+        return this.pautaService.acharPorId(id);
+    }
+
+    public boolean verificaDescricaoRep(Pauta pauta){
+
+        return this.pautaService.listarTodos().get().stream().filter(pautas -> pautas.getDescricaoPauta().equals(pauta.getDescricaoPauta())).findFirst().isPresent();
     }
 }
 
--- cwi/src/main/java/com/cwi/desafio/cwi/business/exceptions/PautaExceptionMessage.java
+++ cwi/src/main/java/com/cwi/desafio/cwi/business/exceptions/PautaExceptionMessage.java
@@ -1,7 +1,8 @@
 package com.cwi.desafio.cwi.business.exceptions;
 
-public abstract class PautaExceptionMessage {
+public  class PautaExceptionMessage extends Throwable {
 
     public static final String NOT_FOUND = "A pauta solicitada não foi encontrada";
+    public static final String FORBIDDEN = "Já existe uma pauta com essa descrição";
 
 }
